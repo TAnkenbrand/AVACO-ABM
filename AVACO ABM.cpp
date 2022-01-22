c41d3e2069b9c5497157aadc040ef23d10b86398
Thomas Ankenbrand <ta@avaco.ch>
2022-01-22 18:45:54 +0100

    Reorg. File structure

--- AVACO ABM.cpp
+++ AVACO ABM.cpp
@@ -3,30 +3,28 @@
 #include <string>
 #include <sstream>
 
-//#include <omp.h> openmp
-
 using namespace std;
 
-double prices[8050][13];
+double prices[8050][16];
 
 #include "AVACO ABM TS.h"
 
 int main()
 {
-	int i, j, a, c, e, f, g, h, n, tt, agentsi[3][5];
+	int i, j, a, c, e, f, g, h, n, nts, tt, agentsi[3][5];
 	double agentsd[3][4], ticker[8050][4];
 	string line, value;
 	double orders, volume, maxperformance, maxdrawdown, efficency, b, d;
 
-	n = 230;	//number of datasets 460 / 8045 / 321 in sample 276 / 5219 / 230
-	tt = 1;	//Dependent/target time series
-
-	
-	ifstream PriceFile("AVACO Price.csv");
+	n = 230;	//number of datasets
+	nts = 16;	//number of time series
+	tt = 15;	//Dependent/target time series
+		
+	ifstream PriceFile("AVACO ABM Price.csv");
 	for (i = 0; i < n; i++) {
 		getline(PriceFile, line);
 		stringstream ss(line);
-		for (j = 0; j < 3; j++) { //13
+		for (j = 0; j < nts; j++) {
 			getline(ss, value, ',');
 			prices[i][j] = stod(value);
 		}
@@ -36,30 +34,30 @@ int main()
 //	ofstream TickerFile("AVACO ABM Ticker.csv");
 	ofstream SolutionFile("AVACO ABM Solution.csv");
 	
-	for (a = 1; a < 13; a++) {
+	for (a = 1; a < 2; a++) { //13
 		for (b = 0; b <= 0.1; b += 0.01) {
 			for (c = 1; c < 13; c++) {
 				for (d = 0; d <= 0.1; d += 0.01) {
 					for (e = 1; e < 5; e++) {
 						for (f = e; f < 5; f++) { //1
-							for (g = 0; g < 3; g++) { //13
-								for (h = 0; h < 3; h++) { //13
+							for (g = 0; g < nts; g++) {
+								for (h = 0; h < nts; h++) {
 									//Initialisation Agent1
-									agentsi[0][0] = e; //e;			//Typ: 1:Trend, 2:Contrarian, 3:Fundamentalist Trend, 4:Fundamentalist Contrarian, 5:Dynamic Optimizer
-									agentsi[0][1] = a; // a;		//Time Lag: 1-12, Time Lag Optimizer: 1 - 25
-									agentsi[0][2] = g; // g;		//Time Series
-									agentsd[0][0] = b; // b;		//Trading Ratio
-									agentsd[0][1] = 1;		//Cash
-									agentsd[0][2] = 0.01;	//Asset
-									agentsd[0][3] = 0;		//Order
+									agentsi[0][0] = e; // e		//Typ: 1:Trend, 2:Contrarian, 3:Fundamentalist Trend, 4:Fundamentalist Contrarian, 5:Dynamic Optimizer
+									agentsi[0][1] = a; // a		//Time Lag: 1-12, Time Lag Optimizer: 1 - 25
+									agentsi[0][2] = g; // g		//Time Series
+									agentsd[0][0] = b; // b		//Trading Ratio
+									agentsd[0][1] = 1;			//Cash
+									agentsd[0][2] = 0.01;		//Asset
+									agentsd[0][3] = 0;			//Order
 //Initialisation Agent2
-									agentsi[1][0] = f; //f 		//Typ: 1:Trend, 2:Contrarian, 3:Fundamentalist Trend, 4:Fundamentalist Contrarian, 5:Dynamic Optimizer
-									agentsi[1][1] = c;	//c		//Time Lag: 1-12
-									agentsi[1][2] = h;	//h		//Time Series
-									agentsd[1][0] = d;	//d		//Trading Ratio
-									agentsd[1][1] = 1;		//Cash
-									agentsd[1][2] = 0.01;	//Asset
-									agentsd[1][3] = 0;		//Order
+									agentsi[1][0] = f;  // f 	//Typ: 1:Trend, 2:Contrarian, 3:Fundamentalist Trend, 4:Fundamentalist Contrarian, 5:Dynamic Optimizer
+									agentsi[1][1] = c;	// c	//Time Lag: 1-12
+									agentsi[1][2] = h;	// h	//Time Series
+									agentsd[1][0] = d;	// d	//Trading Ratio
+									agentsd[1][1] = 1;			//Cash
+									agentsd[1][2] = 0.01;		//Asset
+									agentsd[1][3] = 0;			//Order
 
 									for (i = 0; i < n; i++) {
 										for (j = 0; j < 4; j++) {
@@ -71,23 +69,7 @@ int main()
 									efficency = 0;
 									for (i = 36; i < n; i++) {
 										for (j = 0; j < 3; j++) {
-											if (agentsi[j][0] == 1) agentsi[j][4] = trend(i, 0, agentsi[j][1], agentsi[j][2]);
-											if (agentsi[j][0] == 2) agentsi[j][4] = contrarien(i, 0, agentsi[j][1], agentsi[j][2]);
-											if (agentsi[j][0] == 3) agentsi[j][4] = fundamentalistt(i, 0, agentsi[j][1], agentsi[j][2], tt);
-											if (agentsi[j][0] == 4) agentsi[j][4] = fundamentalistc(i, 0, agentsi[j][1], agentsi[j][2], tt);
-											if (agentsi[j][0] == 5) agentsi[j][4] = doptimizer(i, 0, tt, n, agentsi[j][1]);
-
-
-											if (agentsd[j][1] > 0) {
-												if (agentsi[j][4] == 1) {
-													agentsd[j][3] = agentsd[j][1] * agentsd[j][0] / prices[i][tt];
-												}
-											}
-											if (agentsd[j][2] > 0) {
-												if (agentsi[j][4] == -1) {
-													agentsd[j][3] = -agentsd[j][2] * agentsd[j][0];
-												}
-											}
+											agentsd[j][3] = order_calculation(agentsi[j][0], agentsi[j][1], agentsi[j][2], agentsd[j][0], agentsd[j][1], agentsd[j][2], i, tt, n);
 										}
 
 										//Clearing
@@ -121,8 +103,8 @@ int main()
 									}
 									efficency = ticker[n - 1][2] / (efficency - abs((prices[36][tt] - prices[35][tt]) / prices[35][tt]));
 //									TickerFile.close();
-									if (efficency > 0.29) {
-										if (maxdrawdown < 0.64) {
+									if (efficency > 0.45) {
+										if (maxdrawdown < 0.60) {
 											SolutionFile << efficency << ";" << maxdrawdown << ";" << a << ";" << b << ";" << c << ";" << d << ";" << e << ";" << f << ";" << g << ";" << h << endl;
 										}
 									}
